7544ef63387b080cfd76372b1e21be5c1815d2de
Chironto Rudra Paul <chirontorudrapaul@gmail.com>
2025-12-03 02:00:37 +0600

    suspense  error commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import LoginClient from './LoginClient';
 
 export default function LoginPage() {
   return (
     <main>
       <h1 className="sr-only">Login</h1>
-      <LoginClient />
+      <Suspense fallback={<div>Loading login...</div>}>
+        <LoginClient />
+      </Suspense>
     </main>
   );
 }
